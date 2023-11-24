e19f870b01730506481f445be90d0117e495989b
DayTeaJun <gkfnck@gmail.com>
2023-11-24 19:39:39 +0900

    Feat: React-Router 도입 및 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import GlobalStyles from './GlobalStyled';
 import SearchMain from './Pages/SearchPage/SearchMain';
 
-// 컴포넌트의 리턴값인 JSX에 대한 타입 지정
 function App() {
 	return (
 		<>
--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -1,1 +1,9 @@
-export {};
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+export default function Router() {
+	return (
+		<BrowserRouter basename='/'>
+			<Routes>{/* <Route path='/' element={}></Route> */}</Routes>
+		</BrowserRouter>
+	);
+}
