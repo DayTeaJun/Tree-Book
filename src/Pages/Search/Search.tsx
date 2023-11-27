2d659a6a35cc79b410c5f8609bf69d7aa32acc51
DayTeaJun <gkfnck@gmail.com>
2023-11-27 23:07:50 +0900

    Rename, Docs: Search 컴포넌트명 변경 및 BookDetail 컴포넌트 추가

--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -2,8 +2,9 @@ import { FormEventHandler, useRef, useState } from 'react';
 import { getBooks } from '../../Api/searchApi';
 import { useQuery } from '@tanstack/react-query';
 import { BData } from '../../Types/bookData';
+import { Link } from 'react-router-dom';
 
-export default function SearchMain() {
+export default function Search() {
 	const [searchTitle, setSearchTitle] = useState('');
 	const inputRef = useRef<HTMLInputElement>(null);
 
@@ -33,12 +34,12 @@ export default function SearchMain() {
 			{books && books.length !== 0 ? (
 				<>
 					{books.map((el: BData) => (
-						<div key={el.isbn}>
+						<Link to={'./detail'} state={{ bookData: el }} key={el.isbn}>
 							<img src={el.thumbnail} alt={`책 ${el.title}의 이미지`} />
 							<h2>{el.authors}</h2>
 							<h2>출판사 : {el.publisher}</h2>
 							<p>{el.price}원</p>
-						</div>
+						</Link>
 					))}
 				</>
 			) : (
--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomeFeed from '../Pages/HomeFeed/HomeFeed';
-import SearchMain from '../Pages/SearchPage/SearchMain';
+import BookDetail from '../Pages/BookDetail/BookDetail';
+import Search from '../Pages/Search/Search';
 
 export default function Router() {
 	return (
 		<BrowserRouter basename='/'>
 			<Routes>
 				<Route path='/' element={<HomeFeed />}></Route>
-				<Route path='/search' element={<SearchMain />}></Route>
+				<Route path='/search' element={<Search />}>
+					<Route path=':bookDetail' element={<BookDetail />} />
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
