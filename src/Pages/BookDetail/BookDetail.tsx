f833f3533a766272a604b2a9465acc0f1763b1b2
DayTeaJun <gkfnck@gmail.com>
2023-11-29 23:52:28 +0900

    Fix: 상세페이지의 책 데이터가 중복일 때 최신으로 가져오는 코드를 간결하게 수정

--- src/Pages/BookDetail/BookDetail.tsx
+++ src/Pages/BookDetail/BookDetail.tsx
@@ -13,33 +13,20 @@ export default function BookDetail() {
 
 	return (
 		<>
-			{books ? (
-				books.length > 1 ? (
-					<>
-						<div key={books[books.length - 1].isbn}>
-							<img
-								src={books[books.length - 1].thumbnail}
-								alt={`책 ${books[books.length - 1].title}의 이미지`}
-							/>
-							<h2>{books[books.length - 1].authors}</h2>
-							<h2>출판사 : {books[books.length - 1].publisher}</h2>
-							<p>{books[books.length - 1].price}원</p>
-							<p>{books[books.length - 1].contents}</p>
-							<p>{books[books.length - 1].isbn}</p>
-						</div>
-					</>
-				) : (
-					books.map((el: BData) => (
-						<div key={el.isbn}>
-							<img src={el.thumbnail} alt={`책 ${el.title}의 이미지`} />
-							<h2>{el.authors}</h2>
-							<h2>출판사 : {el.publisher}</h2>
-							<p>{el.price}원</p>
-							<p>{el.contents}</p>
-							<p>{el.isbn}</p>
-						</div>
-					))
-				)
+			{books && books.length !== 0 ? (
+				<>
+					<div key={books[books.length - 1].isbn}>
+						<img
+							src={books[books.length - 1].thumbnail}
+							alt={`책 ${books[books.length - 1].title}의 이미지`}
+						/>
+						<h2>{books[books.length - 1].authors}</h2>
+						<h2>출판사 : {books[books.length - 1].publisher}</h2>
+						<p>{books[books.length - 1].price}원</p>
+						<p>{books[books.length - 1].contents}</p>
+						<p>{books[books.length - 1].isbn}</p>
+					</div>
+				</>
 			) : (
 				<>{books && books.length === 0 && <h2>not found</h2>}</>
 			)}
