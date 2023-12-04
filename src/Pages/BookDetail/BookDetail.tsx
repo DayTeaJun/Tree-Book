afc1241f3d1717e4dad9c73eaf02de4d3b9cf14e
DayTeaJun <gkfnck@gmail.com>
2023-12-04 19:03:21 +0900

    Design: bookDetail 태그명 변경 및 스타일 수정

--- src/Pages/BookDetail/BookDetail.tsx
+++ src/Pages/BookDetail/BookDetail.tsx
@@ -20,18 +20,39 @@ export default function BookDetail() {
 			{books && books.length !== 0 ? (
 				<>
 					<BookDetailSection key={books[0].isbn}>
-						<img
-							src={books[0].thumbnail}
-							alt={`책 ${books[0].title}의 이미지`}
-						/>
 						<div>
-							<h2>작가 | {books[0].authors}</h2>
-							<h2>출판사 | {books[0].publisher}</h2>
-							<p>가격 | {books[0].price}원</p>
-							<p>내용 | {books[0].contents}</p>
-							<p>ISBN | {books[0].isbn}</p>
-							<p>출판일 | {books[0].datetime.substr(0, 10)}</p>
+							<img
+								src={books[0].thumbnail}
+								alt={`책 ${books[0].title}의 이미지`}
+							/>
 							<a onClick={onWebsiteView}>다음 검색으로 이동</a>
+						</div>
+						<div>
+							<h2>{books[0].title}</h2>
+							<dl>
+								<dt>작가</dt>
+								<dd>{books[0].authors}</dd>
+							</dl>
+							<dl>
+								<dt>출판사</dt>
+								<dd>{books[0].publisher}</dd>
+							</dl>
+							<dl>
+								<dt>내용</dt>
+								<dd>{books[0].contents}</dd>
+							</dl>
+							<dl>
+								<dt>가격</dt>
+								<dd>{books[0].price}원</dd>
+							</dl>
+							<dl>
+								<dt>ISBN</dt>
+								<dd>{books[0].isbn}</dd>
+							</dl>
+							<dl>
+								<dt>출판일</dt>
+								<dd>{books[0].datetime.substr(0, 10)}</dd>
+							</dl>
 						</div>
 					</BookDetailSection>
 				</>
--- src/Pages/BookDetail/bookDetail.style.tsx
+++ src/Pages/BookDetail/bookDetail.style.tsx
@@ -6,6 +6,12 @@ export const BookDetailSection = styled.section`
 	max-width: 800px;
 	padding: 20px;
 
+	h2 {
+		font-weight: bold;
+		font-size: 20px;
+		margin-bottom: 5px;
+	}
+
 	img {
 		height: 100%;
 		text-align: center;
@@ -15,7 +21,22 @@ export const BookDetailSection = styled.section`
 		display: flex;
 		flex-direction: column;
 		gap: 8px;
+
+		dl {
+			display: flex;
+			font-weight: 200;
+			dt {
+				flex-shrink: 0;
+				width: 80px;
+				margin-right: 10px;
+				border-right: solid 2px #bab7b6;
+				font-weight: 600;
+			}
+		}
+
 		a {
+			text-align: center;
+			font-size: 13px;
 			cursor: pointer;
 			color: skyblue;
 		}
