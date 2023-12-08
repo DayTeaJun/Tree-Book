6c9de272169d8abbf5719a6de3f5d3e3557bf428
DayTeaJun <gkfnck@gmail.com>
2023-12-08 19:41:35 +0900

    Remove: bookDetail 및 search style book 관련 코드 제거

--- src/Pages/Search/search.style.tsx
+++ src/Pages/Search/search.style.tsx
@@ -1,26 +1,3 @@
 import styled from 'styled-components';
 
-export const SearchSection = styled.section`
-	width: 1080px;
-	padding: 20px;
-	display: flex;
-	justify-content: center;
-	gap: 5px;
-	flex-wrap: wrap;
-
-	a {
-		padding: 10px;
-		width: 150px;
-	}
-
-	img {
-		border-radius: 5px;
-	}
-
-	h2 {
-		text-align: center;
-		font-size: 14px;
-		font-weight: bold;
-		margin-top: 5px;
-	}
-`;
+export {};
