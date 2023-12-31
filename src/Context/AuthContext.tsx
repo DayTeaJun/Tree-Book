f2387f0212673657f641bccb78524244a49b8139
DayTeaJun <gkfnck@gmail.com>
2023-12-31 19:40:12 +0900

    Feat: Context API AuthContext 추가

---
+++ src/Context/AuthContext.tsx
@@ -0,0 +1,60 @@
+import { createContext, ReactNode, useEffect, useReducer } from 'react';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { appAuth } from '../Firebase/config';
+
+interface AuthContextProps {
+	user: User | null;
+	isAuthReady: boolean;
+	dispatch?: React.Dispatch<AuthAction>;
+}
+
+type AuthAction =
+	| { type: 'login'; payload: User }
+	| { type: 'logout' }
+	| { type: 'isAuthReady'; payload: User; isAuthReady?: boolean };
+
+const authReducer = (
+	state: AuthContextProps,
+	action: AuthAction
+): AuthContextProps => {
+	switch (action.type) {
+		case 'login':
+			return { ...state, user: action.payload };
+
+		case 'logout':
+			return { ...state, user: null };
+
+		case 'isAuthReady':
+			return { ...state, user: action.payload, isAuthReady: true };
+
+		default:
+			return state;
+	}
+};
+
+const AuthContext = createContext<AuthContextProps | undefined>(undefined);
+
+interface Props {
+	children: JSX.Element | JSX.Element[];
+}
+
+const AuthContextProvider = ({ children }: Props) => {
+	const [state, dispatch] = useReducer<
+		React.Reducer<AuthContextProps, AuthAction>
+	>(authReducer, { user: null, isAuthReady: false });
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(appAuth, (user) => {
+			dispatch({ type: 'isAuthReady', payload: user! });
+		});
+		return unsubscribe;
+	}, []);
+
+	return (
+		<AuthContext.Provider value={{ ...state, dispatch }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export { AuthContext, AuthContextProvider };
