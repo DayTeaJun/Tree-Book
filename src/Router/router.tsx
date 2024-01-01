548f023648c80055f3b66d1e5f8db0db831ed41c
DayTeaJun <gkfnck@gmail.com>
2024-01-01 11:32:45 +0900

    Feat: 로그인 유무에 따른 Router 설정

--- src/Router/router.tsx
+++ src/Router/router.tsx
@@ -1,25 +1,44 @@
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
+import {
+	BrowserRouter,
+	Navigate,
+	Outlet,
+	Route,
+	Routes,
+} from 'react-router-dom';
 import HomeFeed from '../Pages/HomeFeed/HomeFeed';
 import BookDetail from '../Pages/BookDetail/BookDetail';
 import SearchView from '../Pages/Search/SearchView';
 import TopNavbar from '../Layouts/topNavbar';
 import Signup from '../Pages/LoginSignup/Signup';
 import Login from '../Pages/LoginSignup/Login';
+import { useAuthContext } from '../Hook/FirebaseHook/useAuthContext';
 
 export default function Router() {
+	const { isAuthReady, user } = useAuthContext();
+
 	return (
 		<BrowserRouter basename='/'>
 			<TopNavbar />
 
-			<Routes>
-				<Route path='/' element={<HomeFeed />} />
-				<Route path='/signup' element={<Signup />} />
-				<Route path='/Login' element={<Login />} />
-				<Route path='/search/' element={<Outlet />}>
-					<Route path='detail/:bookDetail' element={<BookDetail />} />
-					<Route path=':searchView' element={<SearchView />} />
-				</Route>
-			</Routes>
+			{isAuthReady ? (
+				<Routes>
+					<Route path='/' element={<HomeFeed />} />
+					<Route
+						path='/signup'
+						element={!user ? <Signup /> : <Navigate replace={true} to='/' />}
+					/>
+					<Route
+						path='/Login'
+						element={!user ? <Login /> : <Navigate replace={true} to='/' />}
+					/>
+					<Route path='/search/' element={<Outlet />}>
+						<Route path='detail/:bookDetail' element={<BookDetail />} />
+						<Route path=':searchView' element={<SearchView />} />
+					</Route>
+				</Routes>
+			) : (
+				'Loading...'
+			)}
 		</BrowserRouter>
 	);
 }
