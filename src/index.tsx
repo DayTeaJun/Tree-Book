b26eea586f9820a286fd00c7a5f2f86855255c8f
DayTeaJun <gkfnck@gmail.com>
2024-01-04 20:28:44 +0900

    Remove: 사용하지 않는 파일(index.css) 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthContextProvider } from './Context/AuthContext';
