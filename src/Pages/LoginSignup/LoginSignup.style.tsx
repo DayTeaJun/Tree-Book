9bec9318840e78da470c929ae951dcfa6bb92dbd
DayTeaJun <gkfnck@gmail.com>
2024-01-15 16:10:08 +0900

    Docs: Styles 폴더 생성 및 웹접근성 코드(a11y) 공통 css 재사용

--- src/Pages/LoginSignup/LoginSignup.style.tsx
+++ src/Pages/LoginSignup/LoginSignup.style.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { a11y } from '../../Styles/Common';
 
 const Form = styled.form`
 	max-width: 360px;
@@ -35,14 +36,7 @@ const ImgInput = styled.input`
 `;
 
 const ImgLabel = styled.label`
-	clip: rect(1px, 1px, 1px, 1px);
-	clip-path: inset(50%);
-	width: 1px;
-	height: 1px;
-	margin: -1px;
-	overflow: hidden;
-	padding: 0;
-	position: absolute;
+	${a11y}
 `;
 
 const Legend = styled.legend`
