0ec8c334f16ffd7a96ed4de86aaba4eaeb4f9167
DayTeaJun <gkfnck@gmail.com>
2024-01-17 06:12:49 +0900

    Refactor: CommentList Style 코드 획일화

--- src/Components/Comments/CommentList.style.tsx
+++ src/Components/Comments/CommentList.style.tsx
@@ -1,1 +1,88 @@
-export {};
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	margin-top: 30px;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	border-radius: 5px;
+	border: 1px solid #ccc;
+	padding: 20px;
+`;
+
+const Img = styled.img`
+	width: 60px;
+	height: 60px;
+	border-radius: 50%;
+	border: none;
+`;
+
+const ContainerImgBtn = styled.div`
+	display: flex;
+	justify-content: space-between;
+`;
+
+const ContainerImgLink = styled.div`
+	display: flex;
+	gap: 10px;
+`;
+
+const ALink = styled(Link)`
+	font-size: 1.1em;
+	font-weight: bold;
+	cursor: pointer;
+`;
+
+const ContainerDateBtn = styled.div`
+	display: flex;
+	justify-content: flex-end;
+`;
+
+const ContainerNameComment = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+`;
+
+const ContainerNameDate = styled.div`
+	display: flex;
+	gap: 20px;
+`;
+
+const PDate = styled.p`
+	font-size: 1.1em;
+	font-weight: bold;
+	color: #a1a1a1;
+`;
+
+const PComment = styled.p`
+	font-size: 1em;
+`;
+
+const Button = styled.button`
+	width: 80px;
+	height: 30px;
+	padding: 5px;
+	font-size: 1em;
+	font-weight: bold;
+	border: none;
+	border-radius: 4px;
+	background-color: skyblue;
+	color: white;
+	cursor: pointer;
+`;
+
+export const CL = {
+	Wrapper,
+	Img,
+	ContainerImgBtn,
+	ContainerImgLink,
+	ALink,
+	ContainerDateBtn,
+	ContainerNameComment,
+	ContainerNameDate,
+	PDate,
+	PComment,
+	Button,
+};
--- src/Components/Comments/CommentList.tsx
+++ src/Components/Comments/CommentList.tsx
@@ -1,18 +1,8 @@
 import { useParams } from 'react-router-dom';
 import { useCollection } from '../../Hook/FirebaseHook/useCollection';
-import {
-	CommentDate,
-	CommentsList,
-	DelOrReport,
-	FlexImgBtn,
-	FlexImgLink,
-	FlexNameComment,
-	FlexNameDate,
-	ProfileImg,
-} from './CommentForm.style';
-import { Link } from 'react-router-dom';
 import { useAuthContext } from '../../Hook/FirebaseHook/useAuthContext';
 import { useFirestore } from '../../Hook/FirebaseHook/useFirestore';
+import { CL } from './CommentList.style';
 
 export function CommentList() {
 	const { documents, error } = useCollection('comments');
@@ -27,45 +17,45 @@ export function CommentList() {
 				documents.map(
 					(comment) =>
 						comment.book === book && (
-							<CommentsList key={comment.uid}>
-								<div>
-									<FlexImgBtn>
-										<FlexImgLink>
-											<ProfileImg
-												src={comment.photoURL}
-												alt={`${comment.displayName}의 프로필 사진입니다.`}
-											/>
+							<CL.Wrapper key={comment.uid}>
+								<CL.ContainerImgBtn>
+									<CL.ContainerImgLink>
+										<CL.Img
+											src={comment.photoURL}
+											alt={`${comment.displayName}의 프로필 사진입니다.`}
+										/>
 
-											<FlexNameComment>
-												<FlexNameDate>
-													{user && user.uid === comment.id ? (
-														<Link to={`/profile`}>{comment.displayName}</Link>
-													) : (
-														<Link
-															to={`/profile/${comment.displayName}`}
-															state={{ id: comment.id }}
-														>
-															{comment.displayName}
-														</Link>
-													)}
-													<CommentDate>{comment.createdTime}</CommentDate>
-												</FlexNameDate>
-												<p>{comment.comments}</p>
-											</FlexNameComment>
-										</FlexImgLink>
-										{(user && user.uid) !== comment.id ? (
-											<DelOrReport type='button'>신고</DelOrReport>
-										) : (
-											<DelOrReport
-												type='button'
-												onClick={() => deleteDocument(comment.uid!)}
-											>
-												삭제
-											</DelOrReport>
-										)}
-									</FlexImgBtn>
-								</div>
-							</CommentsList>
+										<CL.ContainerNameComment>
+											<CL.ContainerNameDate>
+												{user && user.uid === comment.id ? (
+													<CL.ALink to={`/profile`}>
+														{comment.displayName}
+													</CL.ALink>
+												) : (
+													<CL.ALink
+														to={`/profile/${comment.displayName}`}
+														state={{ id: comment.id }}
+													>
+														{comment.displayName}
+													</CL.ALink>
+												)}
+												<CL.PDate>{comment.createdTime}</CL.PDate>
+											</CL.ContainerNameDate>
+											<CL.PComment>{comment.comments}</CL.PComment>
+										</CL.ContainerNameComment>
+									</CL.ContainerImgLink>
+									{(user && user.uid) !== comment.id ? (
+										<CL.Button type='button'>신고</CL.Button>
+									) : (
+										<CL.Button
+											type='button'
+											onClick={() => deleteDocument(comment.uid!)}
+										>
+											삭제
+										</CL.Button>
+									)}
+								</CL.ContainerImgBtn>
+							</CL.Wrapper>
 						)
 				)}
 		</>
