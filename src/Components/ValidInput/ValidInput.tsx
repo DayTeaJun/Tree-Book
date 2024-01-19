aa19b40dcd1410317ea1b78a9ac605e4aa157339
DayTeaJun <gkfnck@gmail.com>
2024-01-20 00:19:52 +0900

    Feat: 회원가입시 email, nickname 중복검사 컴포넌트 분할

---
+++ src/Components/ValidInput/ValidInput.tsx
@@ -0,0 +1,103 @@
+import {
+	ChangeEvent,
+	Dispatch,
+	SetStateAction,
+	useEffect,
+	useState,
+} from 'react';
+import { LS } from '../../Pages/LoginSignup/LoginSignup.style';
+import useDebounce from '../../Hook/useDebounce';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { appFirestore } from '../../Firebase/config';
+
+export interface InputValueType {
+	email: string;
+	password: string;
+	displayName: string;
+}
+
+interface ValidInputProps {
+	setInputValue: Dispatch<SetStateAction<InputValueType>>;
+}
+
+export default function ValidInput({ setInputValue }: ValidInputProps) {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [displayName, setDisplayName] = useState('');
+	const [validEmail, setValidEmail] = useState(' ');
+	const [validName, setValidName] = useState(' ');
+	const userRef = collection(appFirestore, 'user');
+	const debounceEmail = useDebounce<string>(email);
+	const debounceName = useDebounce<string>(displayName);
+
+	const handleData = async (e: ChangeEvent<HTMLInputElement>) => {
+		if (e.target.type === 'email') {
+			setEmail(e.target.value);
+			setInputValue((prev) => ({ ...prev, email: e.target.value }));
+		} else if (e.target.type === 'password') {
+			setPassword(e.target.value);
+			setInputValue((prev) => ({ ...prev, password: e.target.value }));
+		} else if (e.target.type === 'text') {
+			setDisplayName(e.target.value);
+			setInputValue((prev) => ({ ...prev, displayName: e.target.value }));
+		}
+	};
+
+	const validCheck = async (validCheck: string) => {
+		const Query = query(
+			userRef,
+			where(`${validCheck}`, '==', validCheck === 'email' ? email : displayName)
+		);
+		const querySnapshot = await getDocs(Query);
+		if (querySnapshot.docs.length > 0) {
+			if (validCheck === 'email') {
+				setValidEmail('중복된 이메일입니다.');
+			} else {
+				setValidName('중복된 닉네임입니다.');
+			}
+		} else {
+			if (validCheck === 'email') {
+				setValidEmail('');
+			} else {
+				setValidName('');
+			}
+		}
+	};
+
+	useEffect(() => {
+		validCheck('email');
+		validCheck('displayName');
+	}, [debounceEmail, debounceName]);
+
+	return (
+		<>
+			<LS.Label htmlFor='myEmail'>Email</LS.Label>
+			<LS.Input
+				type='email'
+				id='myEmail'
+				required
+				value={email}
+				onChange={handleData}
+			/>
+			<p>{validEmail}</p>
+			<LS.Label htmlFor='myPassword'>Password</LS.Label>
+			<LS.Input
+				type='password'
+				id='myPassword'
+				required
+				value={password}
+				onChange={handleData}
+			/>
+
+			<LS.Label htmlFor='myNickName'>Nickname</LS.Label>
+			<LS.Input
+				type='text'
+				id='myNickName'
+				required
+				value={displayName}
+				onChange={handleData}
+			/>
+			<p>{validName}</p>
+		</>
+	);
+}
--- src/Pages/LoginSignup/Signup.tsx
+++ src/Pages/LoginSignup/Signup.tsx
@@ -1,71 +1,27 @@
-import {
-	ChangeEvent,
-	FormEventHandler,
-	useEffect,
-	useRef,
-	useState,
-} from 'react';
+import { FormEventHandler, useState } from 'react';
 import { LS } from './LoginSignup.style';
 import { useSignup } from '../../Hook/FirebaseHook/useSignup';
 import persImg from '../../Assets/No-img.svg';
 import { ImgPreview } from '../../Hook/useImgPreview';
-import { collection, getDocs, query, where } from 'firebase/firestore';
-import { appFirestore } from '../../Firebase/config';
-import useDebounce from '../../Hook/useDebounce';
+import ValidInput, {
+	InputValueType,
+} from '../../Components/ValidInput/ValidInput';
 
 export default function Signup() {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [displayName, setDisplayName] = useState('');
-	const [validEmail, setValidEmail] = useState(' ');
-	const [validName, setValidName] = useState(' ');
 	const { error, isPending, signup } = useSignup();
 	const { imageSrc, imgUrl, onUpload } = ImgPreview();
-	const userRef = collection(appFirestore, 'user');
-	const debounceEmail = useDebounce<string>(email);
-	const debounceName = useDebounce<string>(displayName);
-
-	const handleData = async (e: ChangeEvent<HTMLInputElement>) => {
-		if (e.target.type === 'email') {
-			setEmail(e.target.value);
-		} else if (e.target.type === 'password') {
-			setPassword(e.target.value);
-		} else if (e.target.type === 'text') {
-			setDisplayName(e.target.value);
-		}
-	};
-
-	const emailValid = async () => {
-		const Query = query(userRef, where('email', '==', email));
-		const querySnapshot = await getDocs(Query);
-		if (querySnapshot.docs.length > 0) {
-			setValidEmail('중복된 이메일입니다.');
-		} else {
-			setValidEmail('');
-		}
-	};
-
-	const nameValid = async () => {
-		const Query = query(userRef, where('displayName', '==', displayName));
-		const querySnapshot = await getDocs(Query);
-		if (querySnapshot.docs.length > 0) {
-			setValidName('중복된 닉네임입니다.');
-		} else {
-			setValidName('');
-		}
-	};
-
-	useEffect(() => {
-		emailValid();
-		nameValid();
-	}, [debounceEmail, debounceName]);
+	const [inputValue, setInputValue] = useState<InputValueType>({
+		email: '',
+		password: '',
+		displayName: '',
+	});
 
 	const handleSubmit: FormEventHandler = (e) => {
 		e.preventDefault();
 		if (imgUrl) {
-			signup({ email, password, displayName, imgUrl });
+			signup({ ...inputValue, imgUrl });
 		} else {
-			signup({ email, password, displayName });
+			signup(inputValue);
 		}
 	};
 
@@ -86,33 +42,7 @@ export default function Signup() {
 						onChange={(e) => onUpload(e)}
 					/>
 				</LS.Container>
-				<LS.Label htmlFor='myEmail'>Email</LS.Label>
-				<LS.Input
-					type='email'
-					id='myEmail'
-					required
-					value={email}
-					onChange={handleData}
-				/>
-				<p>{validEmail}</p>
-				<LS.Label htmlFor='myPassword'>Password</LS.Label>
-				<LS.Input
-					type='password'
-					id='myPassword'
-					required
-					value={password}
-					onChange={handleData}
-				/>
-
-				<LS.Label htmlFor='myNickName'>Nickname</LS.Label>
-				<LS.Input
-					type='text'
-					id='myNickName'
-					required
-					value={displayName}
-					onChange={handleData}
-				/>
-				<p>{validName}</p>
+				<ValidInput setInputValue={setInputValue} />
 				<LS.Button type='submit'>회원가입</LS.Button>
 			</fieldset>
 		</LS.Form>
