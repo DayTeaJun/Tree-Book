956538796f640d95882d897025b78ae4029be80f
DayTeaJun <gkfnck@gmail.com>
2024-02-26 23:58:38 +0900

    Fix: 좋아요 취소한 책 뜨지 않도록 변경

--- src/Api/Firebase/getLikedBooks.ts
+++ src/Api/Firebase/getLikedBooks.ts
@@ -1,22 +1,25 @@
-import { collection, getDocs, query } from 'firebase/firestore';
+import { collection, getDocs, limit, query } from 'firebase/firestore';
 import { appFirestore } from '../../Firebase/config';
 
 export const getLikedBooks = async (book?: string) => {
 	const LikesRef = collection(appFirestore, 'BooksLikes');
-	const likedQuery = query(LikesRef);
+	const likedQuery = query(LikesRef, limit(17));
 	const likedQuerySnapshot = await getDocs(likedQuery);
 	const likedQueryData = likedQuerySnapshot.docs.map((doc) => doc.data());
+	const likedBooks = likedQueryData.filter(
+		(doc) => Object.keys(doc.likeBy).length > 0
+	);
 	if (book === 'best') {
-		likedQueryData.sort(
+		likedBooks.sort(
 			(a, b) => Object.keys(b.likeBy).length - Object.keys(a.likeBy).length
 		);
-		const result = likedQueryData.slice(0, 2);
+		const result = likedBooks.slice(0, 2);
 		return result;
 	} else if (book === 'home') {
-		likedQueryData.sort(
+		likedBooks.sort(
 			(a, b) => Object.keys(b.likeBy).length - Object.keys(a.likeBy).length
 		);
-		const result = likedQueryData.slice(3, 17);
+		const result = likedBooks.slice(3, 17);
 		return result;
 	}
 };
