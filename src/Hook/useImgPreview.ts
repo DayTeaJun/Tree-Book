936e3387e4dc441dc82e932271fcf201649e707c
DayTeaJun <gkfnck@gmail.com>
2024-03-03 22:55:05 +0900

    Feat: 프로필 이미지 업로드 정규식 추가

--- src/Hook/useImgPreview.ts
+++ src/Hook/useImgPreview.ts
@@ -5,20 +5,29 @@ export function ImgPreview() {
 	const [imgUrl, setImgUrl] = useState(null);
 
 	const onUpload = (e: any) => {
+		const fileRegex = /(.*?)\.(jpg|jpeg|png|gif|bmp)$/;
 		const file = e.target.files[0];
-		setImgUrl(file);
-		if (!file) {
+
+		if (file) {
+			const fileEx = '.' + file.name.split('.').pop().toLowerCase();
+			if (fileRegex.test(fileEx)) {
+				setImgUrl(file);
+				const reader = new FileReader();
+				reader.readAsDataURL(file);
+
+				return new Promise<void>((resolve) => {
+					reader.onload = () => {
+						setImageSrc(reader.result || null);
+						resolve();
+					};
+				});
+			} else {
+				alert('이미지 파일만 프로필로 설정할 수 있습니다.');
+				return;
+			}
+		} else {
 			return;
 		}
-		const reader = new FileReader();
-		reader.readAsDataURL(file);
-
-		return new Promise<void>((resolve) => {
-			reader.onload = () => {
-				setImageSrc(reader.result || null);
-				resolve();
-			};
-		});
 	};
 
 	return { imageSrc, imgUrl, onUpload };
