ae316eaabe63db3f7d0c89c7e2c92cf38b665080
DayTeaJun <gkfnck@gmail.com>
2024-03-08 22:20:29 +0900

    Fix: 경고문구 주석처리

--- src/Hook/FirebaseHook/userWithdrawal.ts
+++ src/Hook/FirebaseHook/userWithdrawal.ts
@@ -10,14 +10,15 @@ export const useWithdrawal = () => {
 	const withDrawal = async () => {
 		try {
 			if (!user) {
-				return alert('로그인 후 회원 탈퇴할 수 있습니다!');
+				return;
+				// alert('로그인 후 회원 탈퇴할 수 있습니다!');
 			}
 			await deleteUser(user);
-			alert('정상적으로 계정이 삭제되었습니다!');
+			// alert('정상적으로 계정이 삭제되었습니다!');
 			dispatch({ type: 'logout' });
 			navigate('/');
 		} catch (error) {
-			alert('오류로 인해 계정이 삭제되지 않았습니다!');
+			// alert('오류로 인해 계정이 삭제되지 않았습니다!');
 			console.log(error);
 		}
 	};
