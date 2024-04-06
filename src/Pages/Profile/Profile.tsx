18e6fcc5c89742658d5508bb0b8713ed35e7524c
DayTeaJun <gkfnck@gmail.com>
2024-04-06 21:55:04 +0900

    Fix: Profile SkeletonUI 코드 통합

--- src/Pages/Profile/Profile.tsx
+++ src/Pages/Profile/Profile.tsx
@@ -1,13 +1,13 @@
 import { useParams } from 'react-router-dom';
 import { useAuthContext } from '../../Context/useAuthContext';
 import UserLiked from './UserLiked';
-import { ProfileSekeleton } from './Profile.skeleton';
 import { useQuery } from '@tanstack/react-query';
 import { UserComment } from './UserComment';
 import { Box, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import { getUser } from '../../Api/Firebase/getUser';
+import { Shimmer } from '../../Styles/Common';
 
 export default function Profile() {
 	const { user } = useAuthContext();
@@ -23,27 +23,13 @@ export default function Profile() {
 		queryFn: () => getUser('user', userProfile),
 	});
 
-	if (isLoading) {
-		return (
-			<Box
-				component='main'
-				sx={{
-					display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					padding: '20px 24px',
-					marginTop: '20px',
-					minHeight: '450px',
-					gap: '10px',
-				}}
-			>
-				<ProfileSekeleton />
-			</Box>
-		);
-	}
-
 	return (
 		<>
+			<Helmet>
+				<title>
+					{`${userDocument && userDocument.displayName}님의 프로필 - TreeBook`}
+				</title>
+			</Helmet>
 			<Box
 				component='main'
 				sx={{
@@ -66,11 +52,6 @@ export default function Profile() {
 						gap: '20px',
 					}}
 				>
-					<Helmet>
-						<title>
-							{userDocument && userDocument.displayName}님의 프로필 - TreeBook
-						</title>
-					</Helmet>
 					<Box
 						sx={{
 							minWidth: '30%',
@@ -94,52 +75,94 @@ export default function Profile() {
 								overflow: 'hidden',
 							}}
 						>
-							{userDocument && (
-								<img
-									src={
-										(userId === (user && user.displayName) &&
-											user &&
-											user.photoURL) ||
-										userDocument.photoURL
-									}
-									alt={`${userDocument.displayName}의 프로필 사진입니다.`}
-								/>
+							{isLoading ? (
+								<Shimmer />
+							) : (
+								userDocument && (
+									<img
+										src={
+											(userId === (user && user.displayName) &&
+												user &&
+												user.photoURL) ||
+											userDocument.photoURL
+										}
+										alt={`${userDocument.displayName}의 프로필 사진입니다.`}
+									/>
+								)
 							)}
 						</Box>
-						<Typography component='h2' fontSize='1.5em' fontWeight='bold'>
-							{userDocument && userDocument?.displayName}
-						</Typography>
-						<Typography component='p' fontSize='1em' textAlign='center'>
-							{(userDocument && userDocument?.intro) || ''}
-						</Typography>
-						{userDocument && userId === (user && user.displayName) && (
-							<>
-								<Link to='./edit' state={{ intro: userDocument.intro }}>
-									<Typography
-										component='p'
-										fontSize='1em'
-										textAlign='center'
-										fontWeight='700'
-										color='text.primary'
-										sx={{
-											width: '100%',
-											padding: '10px 20px',
-											borderRadius: '10px',
-											backgroundColor: 'background.book',
-											'&:hover': {
-												backgroundColor: 'background.hover',
-											},
-										}}
-									>
-										프로필 수정
-									</Typography>
-								</Link>
-							</>
+						{isLoading ? (
+							<Box
+								sx={{
+									width: '200px',
+									height: '16px',
+									overflow: 'hidden',
+								}}
+							>
+								<Shimmer />
+							</Box>
+						) : (
+							<Typography component='h2' fontSize='1.5em' fontWeight='bold'>
+								{userDocument && userDocument?.displayName}
+							</Typography>
+						)}
+						{isLoading ? (
+							<Box
+								sx={{
+									width: '200px',
+									height: '36px',
+									overflow: 'hidden',
+								}}
+							>
+								<Shimmer />
+							</Box>
+						) : (
+							<Typography component='p' fontSize='1em' textAlign='center'>
+								{(userDocument && userDocument?.intro) || ''}
+							</Typography>
+						)}
+						{isLoading ? (
+							<Box
+								sx={{
+									width: '200px',
+									height: '36px',
+									overflow: 'hidden',
+								}}
+							>
+								<Shimmer />
+							</Box>
+						) : (
+							userDocument &&
+							userId === (user && user.displayName) && (
+								<>
+									<Link to='./edit' state={{ intro: userDocument.intro }}>
+										<Typography
+											component='p'
+											fontSize='1em'
+											textAlign='center'
+											fontWeight='700'
+											color='text.primary'
+											sx={{
+												width: '100%',
+												padding: '10px 20px',
+												borderRadius: '10px',
+												backgroundColor: 'background.book',
+												'&:hover': {
+													backgroundColor: 'background.hover',
+												},
+											}}
+										>
+											프로필 수정
+										</Typography>
+									</Link>
+								</>
+							)
 						)}
 					</Box>
 					<Box
 						sx={{
 							width: 'calc(70% - 160px)',
+							minHeight: '570px',
 							display: 'flex',
 							flexDirection: 'column',
 							gap: '30px',
