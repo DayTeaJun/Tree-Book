a7e70fe8660c6ed66379fe7d537db1eb383a38c8
DayTeaJun <gkfnck@gmail.com>
2024-04-06 22:35:56 +0900

    Fix: getDocuments 코드 단순화

--- src/Api/Firebase/getDocuments.ts
+++ src/Api/Firebase/getDocuments.ts
@@ -2,37 +2,20 @@ import { collection, getDocs, orderBy, query, where } from 'firebase/firestore';
 import { appFirestore } from '../../Firebase/config';
 import { FirestoreDocument } from '../../Types/firestoreType';
 
-export const getDocuments = async (transaction: string, isbn?: string) => {
-	let documentQuery;
-	if (transaction === ('BooksLikes' || 'comments') && isbn) {
-		documentQuery = query(
-			collection(appFirestore, transaction),
-			where('isbn', '==', isbn)
-		);
-	} else {
-		documentQuery = query(
-			collection(appFirestore, transaction),
-			orderBy('createdTime', 'desc')
-		);
-	}
+export const getDocuments = async (transaction: string, isbn: string) => {
+	const documentQuery = query(
+		collection(appFirestore, transaction),
+		where('isbn', '==', isbn)
+	);
 
 	const documentSnapshot = await getDocs(documentQuery);
 	let result: FirestoreDocument[] = [];
 	documentSnapshot.docs.map((doc) => {
 		const data = doc.data();
-		if (data.createdTime && transaction !== 'comments') {
-			const createdTime = data.createdTime.toDate().toLocaleString();
-			result.push({
-				...data,
-				createdTime: createdTime,
-				uid: doc.id,
-			});
-		} else {
-			result.push({
-				...data,
-				uid: doc.id,
-			});
-		}
+		result.push({
+			...data,
+			uid: doc.id,
+		});
 	});
 
 	return result;
