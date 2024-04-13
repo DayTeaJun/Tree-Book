4dbf98ad8b96f777e3321e4f98400f222c7aea83
DayTeaJun <gkfnck@gmail.com>
2024-04-13 22:53:28 +0900

    Design: Footer 임시 추가

---
+++ src/Layouts/Footer/Footer.tsx
@@ -0,0 +1,43 @@
+import { Box, Grid, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+export const Footer = () => {
+	return (
+		<Box
+			component='footer'
+			sx={{
+				width: '100%',
+				bottom: '0',
+				backgroundColor: 'background.book',
+			}}
+		>
+			<Box
+				component='nav'
+				sx={{
+					padding: '10px 0',
+					textAlign: 'center',
+				}}
+			>
+				<Box
+					component='a'
+					fontWeight='bold'
+					color='text.primary'
+					sx={{ cursor: 'pointer' }}
+					onClick={() => {
+						window.open('https://github.com/DayTeaJun');
+					}}
+				>
+					<GitHubIcon fontSize='large' />
+				</Box>
+				<Typography
+					component='p'
+					fontSize='0.9em'
+					sx={{ display: 'flex', gap: '10px', justifyContent: 'center' }}
+				>
+					ⓒ DayTeaJun.
+					<br /> All Rights Reserved.
+				</Typography>
+			</Box>
+		</Box>
+	);
+};
