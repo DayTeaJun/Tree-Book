ed738c14cd07d2a1d54a092ba0956dea4d429f1d
DayTeaJun <gkfnck@gmail.com>
2024-04-29 00:42:35 +0900

    Feat: 리뷰 작성 날짜 시간 몇분전 표시 기능 추가

---
+++ src/Hook/useImgPreview.ts
@@ -0,0 +1,36 @@
+import { useSnackbar } from 'notistack';
+import { useState } from 'react';
+
+export function ImgPreview() {
+	const [imageSrc, setImageSrc]: any = useState(null);
+	const [imgUrl, setImgUrl] = useState(null);
+	const { enqueueSnackbar } = useSnackbar();
+
+	const onUpload = (e: any) => {
+		const fileRegex = /(.*?)\.(jpg|jpeg|png|gif|bmp)$/;
+		const file = e.target.files[0];
+
+		if (file) {
+			const fileEx = '.' + file.name.split('.').pop().toLowerCase();
+			if (fileRegex.test(fileEx)) {
+				setImgUrl(file);
+				const reader = new FileReader();
+				reader.readAsDataURL(file);
+
+				return new Promise<void>((resolve) => {
+					reader.onload = () => {
+						setImageSrc(reader.result || null);
+						resolve();
+					};
+				});
+			} else {
+				enqueueSnackbar('이미지만 등록할 수 있습니다!', { variant: 'error' });
+				return;
+			}
+		} else {
+			return;
+		}
+	};
+
+	return { imageSrc, imgUrl, onUpload };
+}
